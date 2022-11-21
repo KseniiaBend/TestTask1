ff7bb96945e506656545c909be26729ce4c0cf0d
Kseniya Bendyukova <kseniya.bendukova@gs-labs.ru>
2022-11-21 21:52:11 +0200

    fix: fix

--- src/hooks/useLoadData.tsx
+++ src/hooks/useLoadData.tsx
@@ -1,19 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useCallback, useEffect } from 'react';
 import { setLoading, setError } from 'src/redux/actions/app';
 import { NOT_FOUND } from 'src/utils/constants';
 import { useLoadDataProps } from './types';
-import { selectHasError } from 'src/redux/selectors/app';
 
 const useLoadData = ({ url, model, action, needRender = true }: useLoadDataProps): void => {
   const dispatch = useDispatch();
-  const hasError = useSelector(selectHasError);
 
   const fetchData = useCallback(async () => {
     dispatch(setLoading(true));
-    if (hasError) {
-      dispatch(setError({ hasError: false, errorStatus: '' }));
-    }
+    dispatch(setError({ hasError: false, errorStatus: '' }));
 
     const response = await fetch(url);
 
@@ -26,7 +22,7 @@ const useLoadData = ({ url, model, action, needRender = true }: useLoadDataProps
 
     dispatch(setLoading(false));
     dispatch(setError({ hasError: true, errorStatus: String(response.status) }));
-  }, [action, dispatch, hasError, model, url]);
+  }, [action, dispatch, model, url]);
 
   useEffect(() => {
     if (!needRender) return;
